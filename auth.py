db8b7a9d569e1dcd1c74e5ab33823794ff3cb6f0
root <root@Majeed>
2023-01-26 20:53:51 +0300

    updates

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'majeed.us.auth0.com'
+AUTH0_DOMAIN = 'fsndprojects.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'capstone'
 
